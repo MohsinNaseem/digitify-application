b85a8cdf8d4d69e3fb7978a4c3966ea321fc123d
MohsinNaseem <perfect_889@hotmail.com>
2024-09-02 03:25:52 +0500

    added deploy step in workflow

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Variables
-NAMESPACE="default"  # Change to your desired namespace
-RELEASE_NAME="my-release"  # Change to your desired release name
-IMAGE_NAME=$IMAGE_NAME  # Your Docker image name
-IMAGE_TAG=$IMAGE_TAG  # Your Docker image tag
-
 # Update kubeconfig for EKS
+echo "eks cluster updating"
 aws eks update-kubeconfig --region $AWS_REGION --name $EKS_CLUSTER_NAME
 
 # Deploy using Helm
-helm upgrade --install $RELEASE_NAME ./helm \
-  --namespace $NAMESPACE \
-  --set image.repository=$IMAGE_NAME \
-  --set image.tag=$IMAGE_TAG \
-  --wait
+echo "deploying helm on $RELEASE_NAME"
+helm upgrade --install $RELEASE_NAME ./helm --wait
