ccd028038fcce87047194b76494316eb4a4a5230
MohsinNaseem <perfect_889@hotmail.com>
2024-09-02 03:56:39 +0500

    adjust deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Update kubeconfig for EKS
-echo "eks cluster updating"
-aws eks update-kubeconfig --region $AWS_REGION --name $EKS_CLUSTER_NAME
+echo "eks cluster updating for $AWS_REGION"
+aws eks update-kubeconfig --region $AWS_REGION --name $EKS_CLUSTER_NAME || echo "Warning: Failed to update kubeconfig for EKS cluster $EKS_CLUSTER_NAME"
 
 # Deploy using Helm
 echo "deploying helm on $RELEASE_NAME"
-helm upgrade --install $RELEASE_NAME ./helm --wait
+helm upgrade --install $RELEASE_NAME ./helm --wait || echo "Warning: Failed to deploy on EKS cluster for $RELEASE_NAME"
